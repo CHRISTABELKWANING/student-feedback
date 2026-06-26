769e93b89578aa2cf5b808ba2899b68d1a00afe0
Kwaning Christabelgit config --global user.email yaakwaning200@gmail.com <yaakwaning200@gmail.comgit config --global user.name Kwaning>
2026-06-26 18:24:54 +0000

    update HTML and implementing feedback form with FastAPI integration

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from fastapi import FastAPI
-from fastapi import HTMLResponse
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 def home():
     with open("index.html", "r", encoding="utf-8") as file:
-        return file.read()
+        return HTMLResponse(content=file.read())
 
-@app.post("/feedback")
+@app.post("/submit-feedback")
 def feedback():
+    def feedback(recommend: str = Form(...)):
+        return {"recommend" : recommend}
+    
     #Receive the data
 
     #Process the data
@@ -17,5 +22,5 @@ def feedback():
     #Send a response
 
     return {
-        "message": "Feedback submitted successfully!"
+        "message": "Feedback submitted successfully"
     }
